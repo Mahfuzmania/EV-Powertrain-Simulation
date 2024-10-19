b405b54dced1c4891a5e3644877ca03195f6ec63
Md. Mahfuzul Islam <mahfuzulislam41@gmail.com>
2024-10-19 07:02:48 +0600

    Add files via upload

---
+++ bat_cal.m
@@ -0,0 +1,10 @@
+
+weight_per_series_branch_kg = 16.7609;
+
+no_cells_parallel = 46;
+cell_cap_Ah = 4.8;
+
+calculated_bat_weight = weight_per_series_branch_kg * no_cells_parallel;
+
+
+gross_weight_kg = 1232 + calculated_bat_weight
